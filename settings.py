cbc7672dfefcbdc6f253f1bb0bef3a9c87769ad5
Sean Song <seansongss@gmail.com>
2025-05-22 17:46:43 -0400

    add env variables for AWS credentials

--- settings.py
+++ settings.py
@@ -3,8 +3,9 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     S3_BUCKET: str
-    # AWS_ACCESS_KEY_ID: str
-    # AWS_SECRET_ACCESS_KEY: str
+    AWS_ACCESS_KEY_ID: str
+    AWS_SECRET_ACCESS_KEY: str
+    AWS_DEFAULT_REGION: str
 
     class Config:
         env_file = ".env"
